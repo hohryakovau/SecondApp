87fa1246c30e585775bfeb9c3898fef70145bdc1
1 <1>
2022-03-09 23:02:23 +0300

    Initial commit

--- main.go
+++ main.go
@@ -3,30 +3,16 @@ package main
 import "fmt"
 
 func main() {
-<<<<<<< HEAD
-	var A []int = []int{9, 3, 9, 3, 9, 7, 9}
-	fmt.Println("Результат: нету пары у ", Solution(A))
-=======
+
 	var B []int = []int{1, 1, 3, 9, 3, 9, 7, 9, 9, 3, 3, 3, 3, 3, 3, 3, 3}
 
 	fmt.Println(Solution(B))
 
->>>>>>> 0c262eb (Initial commit)
 }
 
 func Solution(A []int) int {
 	len := len(A)
 	var res int
-<<<<<<< HEAD
-	for i := 0; i < len; i++ {
-		for j := 0; j < len; j++ {
-			if A[i] == A[j] {
-				break
-			} else {
-				res = A[i]
-			}
-		}
-=======
 	var z int
 	var arr = make([]int, len)
 	for i := 0; i < len; i++ {
@@ -42,7 +28,7 @@ func Solution(A []int) int {
 		if x == 1 {
 			res = A[y]
 		}
->>>>>>> 0c262eb (Initial commit)
 	}
 	return res
+
 }
